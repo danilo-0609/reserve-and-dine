28dca157ea7427e55e275fe84afb8f63cdebc30c
Danilo Usuga <daniusugaj@gmail.com>
2024-03-01 14:20:41 -0500

    adding common interfaces services

---
+++ src/Modules/Dinners/Application/Common/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Dinners.Application.Common;
+
+public interface ICommand<out TResponse> : IRequest<TResponse> 
+    where TResponse : IErrorOr
+{
+}
---
+++ src/Modules/Dinners/Application/Common/ICommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Dinners.Application.Common;
+
+public interface ICommandHandler<in TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+    where TRequest : ICommand<TResponse>
+    where TResponse : IErrorOr
+{
+}
