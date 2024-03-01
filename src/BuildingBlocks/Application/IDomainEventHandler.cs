1550576c9abd952cbc368c0677262d411ff5a000
Danilo Usuga <daniusugaj@gmail.com>
2024-03-01 08:18:32 -0500

    Adding interface service for handling domain events

---
+++ src/BuildingBlocks/Application/IDomainEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Domain.Events;
+using MediatR;
+
+namespace BuildingBlocks.Application;
+
+public interface IDomainEventHandler<in TNotification> : INotificationHandler<TNotification>
+    where TNotification : IDomainEvent
+{
+}
