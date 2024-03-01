46cae6490e4ae0c75a062b3d0c5c572e85617993
Danilo Usuga <daniusugaj@gmail.com>
2024-03-01 14:53:12 -0500

    Adding business rules for restaurant entity

---
+++ src/Modules/Dinners/Domain/Restaurants/Rules/TableCannotBeReservedAtCertainTimeWhenTableIsReservedAtThatTimeNowRule.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.Domain.Rules;
+using Dinners.Domain.Common;
+using Dinners.Domain.Restaurants.RestaurantTables;
+using ErrorOr;
+
+namespace Dinners.Domain.Restaurants.Rules;
+
+internal sealed class TableCannotBeReservedAtCertainTimeWhenTableIsReservedAtThatTimeNowRule : IBusinessRule
+{
+    private readonly List<RestaurantTable> _restaurantTables;
+    private readonly DateTime _reservationRequestDateTime;
+    private readonly TimeRange _reservationRequestTimeRange;
+
+    public TableCannotBeReservedAtCertainTimeWhenTableIsReservedAtThatTimeNowRule(List<RestaurantTable> restaurantTables, 
+        DateTime reservationRequestDateTime, 
+        TimeRange reservationRequestTimeRange)
+    {
+        _restaurantTables = restaurantTables;
+        _reservationRequestDateTime = reservationRequestDateTime;
+        _reservationRequestTimeRange = reservationRequestTimeRange;
+    }
+
+    public Error Error => throw new NotImplementedException();
+
+    public bool IsBroken() => _restaurantTables
+        .Any(r => r.ReservedHours
+            .ContainsKey(_reservationRequestDateTime)
+        && r.ReservedHours.Values.Any(i => i.Start == _reservationRequestTimeRange.Start));
+
+    public static string Message => "Table cannot be reserved at certain time when table is reserved at that time";
+}
---
+++ src/Modules/Dinners/Domain/Restaurants/Rules/TableCannotReserveWhenRestaurantIsClosedRule.cs
@@ -0,0 +1,49 @@
+﻿using BuildingBlocks.Domain.Rules;
+using Dinners.Domain.Common;
+using Dinners.Domain.Restaurants.Errors;
+using Dinners.Domain.Restaurants.RestaurantSchedules;
+using ErrorOr;
+
+namespace Dinners.Domain.Restaurants.Rules;
+
+internal sealed class TableCannotReserveWhenRestaurantIsClosedRule : IBusinessRule
+{
+    private readonly RestaurantSchedule _restaurantSchedule;
+    private readonly TimeRange _reservationTimeRangeRequested;
+    private readonly DateTime _reservationDateTimeRequested;
+
+    public TableCannotReserveWhenRestaurantIsClosedRule(RestaurantSchedule restaurantSchedule, 
+        TimeRange reservationTimeRangeRequested, 
+        DateTime reservationDateTimeRequested)
+    {
+        _restaurantSchedule = restaurantSchedule;
+        _reservationTimeRangeRequested = reservationTimeRangeRequested;
+        _reservationDateTimeRequested = reservationDateTimeRequested;
+    }
+
+    public Error Error => RestaurantErrorCodes.CannotReserveWhenRestaurantIsClosed;
+
+    public bool IsBroken()
+    {
+        if (IsRestaurantOpenForReservation())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool IsRestaurantOpenForReservation()
+    {
+        if (_restaurantSchedule.Days.Contains(_reservationDateTimeRequested.DayOfWeek)
+            && _restaurantSchedule.HoursOfOperation.Start <= _reservationTimeRangeRequested.Start
+            && _restaurantSchedule.HoursOfOperation.End > _reservationTimeRangeRequested.End)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static string Message => "Cannot reserve when restaurant is closed";
+}
