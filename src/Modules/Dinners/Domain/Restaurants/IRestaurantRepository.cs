05b17b569da343a268ab5915a3b6784ed45f113d
Danilo Usuga <daniusugaj@gmail.com>
2024-03-01 14:52:35 -0500

    Adding repository service

--- src/Modules/Dinners/Domain/Restaurants/IRestaurantRepository.cs
+++ src/Modules/Dinners/Domain/Restaurants/IRestaurantRepository.cs
@@ -1,5 +1,12 @@
-﻿namespace Dinners.Domain.Restaurants;
+﻿using Domain.Restaurants;
+
+namespace Dinners.Domain.Restaurants;
 
 public interface IRestaurantRepository
 {
+    Task<bool> ExistsAsync(RestaurantId restaurantId);
+
+    Task<Restaurant?> GetRestaurantById(RestaurantId restaurantId);
+
+    Task UpdateAsync(Restaurant restaurant);    
 }
