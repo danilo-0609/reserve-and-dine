07e2853ff57281075fff9c794216cce1f827b03f
Danilo Usuga <daniusugaj@gmail.com>
2024-03-01 14:29:10 -0500

    Adding static customed menu errors

---
+++ src/Modules/Dinners/Domain/Menus/Errors/MenuErrorCodes.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+
+namespace Dinners.Domain.Menus.Errors;
+
+public static class MenuErrorCodes
+{
+    public static Error NotFound =>
+        Error.NotFound("Menu.NotFound", "Menu was not found");
+}
