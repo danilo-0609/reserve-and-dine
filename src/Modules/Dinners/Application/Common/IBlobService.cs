6b6984fad8d7bef0245ec0fd17e9ebf48090d934
Danilo Usuga <daniusugaj@gmail.com>
2024-03-02 18:48:52 -0500

    Adding blob service interface

---
+++ src/Modules/Dinners/Application/Common/IBlobService.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Application;
+
+namespace Dinners.Application.Common;
+
+public interface IBlobService
+{
+    public Task<BlobObject?> GetBlobAsync(string name);
+
+    public Task<string> UploadFileBlobAsync(string filePath, string fileName);
+
+    public Task DeleteBlobAsync(string name);
+}
