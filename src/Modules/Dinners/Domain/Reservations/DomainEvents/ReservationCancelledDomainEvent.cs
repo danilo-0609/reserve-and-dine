daffa26a7fbcdf77cfaae942cb44690934ab2333
Danilo Usuga <daniusugaj@gmail.com>
2024-03-02 18:59:05 -0500

    Adding neccessary fields for the domain event

--- src/Modules/Dinners/Domain/Reservations/DomainEvents/ReservationCancelledDomainEvent.cs
+++ src/Modules/Dinners/Domain/Reservations/DomainEvents/ReservationCancelledDomainEvent.cs
@@ -1,8 +1,12 @@
 ﻿using BuildingBlocks.Domain.Events;
+using Dinners.Domain.Restaurants;
 
 namespace Dinners.Domain.Reservations.DomainEvents;
 
 public sealed record ReservationCancelledDomainEvent(
     Guid DomainEventId,
     ReservationId ReservationId,
+    RestaurantId RestaurantId,
+    int NumberOfTable,
+    DateTime ReservationDateTime,
     DateTime OcurredOn) : IDomainEvent;
