bf7d566f95bd5e15d099d83545ebfd84423e8dbf
Danilo Usuga <daniusugaj@gmail.com>
2024-03-02 18:50:05 -0500

    Adding query common building block

---
+++ src/Modules/Dinners/Application/Common/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Dinners.Application.Common;
+
+public interface IQuery<out TResponse> : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+}
---
+++ src/Modules/Dinners/Application/Common/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Dinners.Application.Common;
+
+public interface IQueryHandler<in TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+    where TRequest : IQuery<TResponse>
+    where TResponse : IErrorOr
+{
+}
