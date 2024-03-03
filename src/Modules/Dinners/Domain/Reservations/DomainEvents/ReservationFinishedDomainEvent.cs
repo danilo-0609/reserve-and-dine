4e889161fce06c203e37079370745379769dfea0
Danilo Usuga <daniusugaj@gmail.com>
2024-03-02 19:00:15 -0500

    Adding reservation finished domain event

---
+++ src/Modules/Dinners/Domain/Reservations/DomainEvents/ReservationFinishedDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Domain.Events;
+using Dinners.Domain.Restaurants;
+
+namespace Dinners.Domain.Reservations.DomainEvents;
+
+public sealed record ReservationFinishedDomainEvent(
+    Guid DomainEventId,
+    ReservationId ReservationId,
+    RestaurantId RestaurantId,
+    Guid ClientId,
+    int NumberOfTable, 
+    DateTime OcurredOn) : IDomainEvent;
