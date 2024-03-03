2dbdd3f0bb7fd23021cbfe638ea6a213699fb892
Danilo Usuga <daniusugaj@gmail.com>
2024-03-02 19:10:48 -0500

    Deleting restaurant id field

--- src/Modules/Dinners/Domain/Restaurants/RestaurantUsers/RestaurantAdministration.cs
+++ src/Modules/Dinners/Domain/Restaurants/RestaurantUsers/RestaurantAdministration.cs
@@ -6,23 +6,19 @@ public sealed record RestaurantAdministration
 
     public Guid AdministratorId { get; private set; }
 
-    public RestaurantId RestaurantId { get; private set; }
-
     public string AdministratorTitle { get; private set; }
 
-    private RestaurantAdministration(Guid administratorId, RestaurantId restaurantId, string name, string administratorTitle)
+    private RestaurantAdministration(Guid administratorId, string name, string administratorTitle)
     {
         AdministratorId = administratorId;
-        RestaurantId = restaurantId;
         Name = name;
         AdministratorTitle = administratorTitle;
     }
 
     public static RestaurantAdministration Create(string name,
         Guid administratorId,
-        RestaurantId restaurantId,
         string administratorTitle)
     {
-        return new RestaurantAdministration(administratorId, restaurantId, name, administratorTitle);
+        return new RestaurantAdministration(administratorId, name, administratorTitle);
     }
 }
