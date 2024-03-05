57bbd3d84383b46006ac4ae698c32ff1375d8293
Danilo Usuga <daniusugaj@gmail.com>
2024-03-05 11:33:16 -0500

    Adding neccessary properties

--- src/Modules/Dinners/Domain/Reservations/DomainEvents/ReservationRequestedDomainEvent.cs
+++ src/Modules/Dinners/Domain/Reservations/DomainEvents/ReservationRequestedDomainEvent.cs
@@ -1,10 +1,15 @@
 ﻿using BuildingBlocks.Domain.Events;
+using Dinners.Domain.Common;
+using Dinners.Domain.Restaurants;
 
 namespace Dinners.Domain.Reservations.DomainEvents;
 
 public sealed record ReservationRequestedDomainEvent(
     Guid DomainEventId,
     ReservationId ReservationId,
+    RestaurantId RestaurantId,
     Guid ClientId,
     int TableNumber,
+    TimeRange ReservationTimeRange,
+    DateTime ReservationDateTimeRequested,
     DateTime OcurredOn) : IDomainEvent;
