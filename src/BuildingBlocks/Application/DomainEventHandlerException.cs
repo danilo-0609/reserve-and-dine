b576617056a301d489ba7b83942595f79e0bfa23
Danilo Usuga <daniusugaj@gmail.com>
2024-03-05 10:59:31 -0500

    Creating custom exception for handling specific errors

---
+++ src/BuildingBlocks/Application/DomainEventHandlerException.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+
+namespace BuildingBlocks.Application;
+
+public sealed class DomainEventHandlerException : Exception
+{
+    public Error Error { get; private set; }
+
+    public DateTime OcurredOn { get; private set; }
+
+    public DomainEventHandlerException(Error error,
+        DateTime ocurredOn) : base(error.Description)
+    {
+        Error = error;
+        OcurredOn = ocurredOn;
+    }
+}
