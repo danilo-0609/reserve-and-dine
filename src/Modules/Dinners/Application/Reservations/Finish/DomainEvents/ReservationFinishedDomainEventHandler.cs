01b8cffcc6b73407d4532621394abd9bfc5eefd6
Danilo Usuga <daniusugaj@gmail.com>
2024-03-05 11:14:44 -0500

    Implementing unit of work pattern andcustom domain event handler exception

--- src/Modules/Dinners/Application/Reservations/Finish/DomainEvents/ReservationFinishedDomainEventHandler.cs
+++ src/Modules/Dinners/Application/Reservations/Finish/DomainEvents/ReservationFinishedDomainEventHandler.cs
@@ -1,6 +1,8 @@
 ﻿using BuildingBlocks.Application;
+using Dinners.Application.Common;
 using Dinners.Domain.Reservations.DomainEvents;
 using Dinners.Domain.Restaurants;
+using Dinners.Domain.Restaurants.Errors;
 using Domain.Restaurants;
 
 namespace Dinners.Application.Reservations.Finish.DomainEvents;
@@ -8,25 +10,33 @@ namespace Dinners.Application.Reservations.Finish.DomainEvents;
 internal sealed class ReservationFinishedDomainEventHandler : IDomainEventHandler<ReservationFinishedDomainEvent>
 {
     private readonly IRestaurantRepository _restaurantRepository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public ReservationFinishedDomainEventHandler(IRestaurantRepository restaurantRepository)
+    public ReservationFinishedDomainEventHandler(IRestaurantRepository restaurantRepository, IUnitOfWork unitOfWork)
     {
         _restaurantRepository = restaurantRepository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task Handle(ReservationFinishedDomainEvent notification, CancellationToken cancellationToken)
     {
         Restaurant? restaurant = await _restaurantRepository.GetRestaurantById(notification.RestaurantId);
 
-        restaurant!.AddRestaurantClient(notification.ClientId);
+        if (restaurant is null)
+        {
+            throw new DomainEventHandlerException(RestaurantErrorCodes.NotFound, DateTime.UtcNow);
+        }
 
-        var tableFree = restaurant!.FreeTable(notification.NumberOfTable);
+        restaurant.AddRestaurantClient(notification.ClientId);
+
+        var tableFree = restaurant.FreeTable(notification.NumberOfTable);
 
         if (tableFree.IsError)
         {
-            throw new Exception("Table was not found");
+            throw new DomainEventHandlerException(tableFree.FirstError, DateTime.UtcNow);
         }
 
         await _restaurantRepository.UpdateAsync(restaurant);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
     }
 }
