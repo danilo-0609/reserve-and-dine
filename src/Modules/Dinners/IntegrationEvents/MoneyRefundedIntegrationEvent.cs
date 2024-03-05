ac608c012f4f15aab520b70df0a97eb4e920c51d
Danilo Usuga <daniusugaj@gmail.com>
2024-03-05 11:23:01 -0500

    Adding Integration events project to Dinners module

---
+++ src/Modules/Dinners/IntegrationEvents/MoneyRefundedIntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using BuildingBlocks.Application;
+
+namespace Dinners.IntegrationEvents;
+
+public sealed record MoneyRefundedIntegrationEvent(Guid IntegrationEventId,
+    Guid RefundId,
+    Guid ReservationId,
+    Guid ClientId,
+    decimal Price,
+    string Currency,
+    DateTime OcurredOn) : IntegrationEvent(IntegrationEventId, OcurredOn);
---
+++ src/Modules/Dinners/IntegrationEvents/ReservationPayedIntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using BuildingBlocks.Application;
+
+namespace Dinners.IntegrationEvents;
+
+public sealed record ReservationPayedIntegrationEvent(Guid IntegrationEventId,
+    Guid ReservationPaymentId,
+    Guid ReservationId,
+    Guid ClientId,
+    decimal Price,
+    string Currency,
+    DateTime OcurredOn) : IntegrationEvent(IntegrationEventId, OcurredOn);
