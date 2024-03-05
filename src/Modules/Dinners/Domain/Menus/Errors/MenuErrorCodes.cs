5d453816bd4b199c6df988a0220e2efd08f34c3e
Danilo Usuga <daniusugaj@gmail.com>
2024-03-05 11:30:15 -0500

    Adding static error

--- src/Modules/Dinners/Domain/Menus/Errors/MenuErrorCodes.cs
+++ src/Modules/Dinners/Domain/Menus/Errors/MenuErrorCodes.cs
@@ -6,4 +6,7 @@ public static class MenuErrorCodes
 {
     public static Error NotFound =>
         Error.NotFound("Menu.NotFound", "Menu was not found");
+
+    public static Error ImagesNotFound =>
+        Error.NotFound("Menu.ImagesNotFound", "Menu images were not found");
 }
