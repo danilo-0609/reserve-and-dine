a38725a02e42b0444fc3b7f1e5110e1472a16d44
Danilo Usuga <daniusugaj@gmail.com>
2024-03-05 11:37:15 -0500

    Adding static error

--- src/Modules/Dinners/Domain/Reservations/Payments/Errors/ReservationPaymentsErrorsCodes.cs
+++ src/Modules/Dinners/Domain/Reservations/Payments/Errors/ReservationPaymentsErrorsCodes.cs
@@ -5,6 +5,9 @@ namespace Dinners.Domain.Reservations.ReservationsPayments.Errors;
 
 public static class ReservationPaymentsErrorsCodes
 {
+    public static Error NotFound =>
+        Error.NotFound("ReservationPayment.NotFound", "Reservation payment was not found");
+
     public static Error CannotPayWhenStatusIsNotRequested =>
         Error.Validation("Payment.CannotPayWhenStatusIsNotConfirmed", PaymentCannotBeMadeWhenReservationStatusIsNotRequestedRule.Message);
 }
