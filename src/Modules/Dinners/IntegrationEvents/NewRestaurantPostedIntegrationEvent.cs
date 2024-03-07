dfd405ca7c7ab2aaa4e81a4d3edfc9e82244facf
Danilo Usuga <daniusugaj@gmail.com>
2024-03-07 11:10:55 -0500

    Adding integration event

---
+++ src/Modules/Dinners/IntegrationEvents/NewRestaurantPostedIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.Application;
+
+namespace Dinners.IntegrationEvents;
+
+public sealed record NewRestaurantPostedIntegrationEvent(Guid IntegrationEventId,
+    Guid RestaurantId,
+    Guid ClientId,
+    DateTime OcurredOn) : IntegrationEvent(IntegrationEventId, OcurredOn);
