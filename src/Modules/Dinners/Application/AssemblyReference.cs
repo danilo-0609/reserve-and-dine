5cd66f8249e60b08ae1061c4db3f550abe91c798
Danilo Usuga <daniusugaj@gmail.com>
2024-03-07 11:02:48 -0500

    Installing application services

---
+++ src/Modules/Dinners/Application/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Dinners.Application;
+
+internal sealed class AssemblyReference
+{
+    internal static Assembly Assembly => typeof(AssemblyReference).Assembly;
+}
--- src/Modules/Dinners/Application/DependencyInjection.cs
+++ src/Modules/Dinners/Application/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Services;
+using Dinners.Application.Services;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Dinners.Application;
 
@@ -6,7 +10,28 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        //MediatR servicess
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssemblyContaining<AssemblyReference>();
+        });
 
+        //Logging services
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(DinnersLoggingBehavior<,>));
+
+        //Validator services
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
+
+        //Unit of work services
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(UnitOfWorkBehavior<,>));
+
+        services.AddValidatorsFromAssembly(AssemblyReference.Assembly, includeInternalTypes: true);
 
         return services;
     }
