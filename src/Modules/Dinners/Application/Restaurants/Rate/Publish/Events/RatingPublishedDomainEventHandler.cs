ed306006998c78dc55bf516dae6a6f042c37765a
Danilo Usuga <daniusugaj@gmail.com>
2024-03-15 09:23:33 -0500

    Adding domain event handler for rating publish domain event

---
+++ src/Modules/Dinners/Application/Restaurants/Rate/Publish/Events/RatingPublishedDomainEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.Application;
+using Dinners.Application.Common;
+using Dinners.Domain.Restaurants.RestaurantRatings.Events;
+using Dinners.IntegrationEvents;
+
+namespace Dinners.Application.Restaurants.Rate.Publish.Events;
+
+internal sealed class RatingPublishedDomainEventHandler : IDomainEventHandler<RatingPublishedDomainEvent>
+{
+    private readonly IEventBus _eventBus;
+
+    public RatingPublishedDomainEventHandler(IEventBus eventBus)
+    {
+        _eventBus = eventBus;
+    }
+
+    public async Task Handle(RatingPublishedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        await _eventBus.PublishAsync(new RatingPublishedIntegrationEvent(notification.DomainEventId,
+            notification.Id.Value,
+            notification.ClientId,
+            notification.Stars,
+            notification.RestaurantId.Value,
+            notification.RestaurantTitle,
+            DateTime.UtcNow));
+    }
+}
