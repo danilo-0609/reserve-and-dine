b7d9f0198cc9495df53c6278e8d32b05796fc9ea
Danilo Usuga <daniusugaj@gmail.com>
2024-03-15 09:50:17 -0500

    Adding ReservationMenus entity for representing the relationship between reservations and menus

---
+++ src/Modules/Dinners/Infrastructure/Domain/ReservationsMenus/ReservationMenus.cs
@@ -0,0 +1,11 @@
+﻿using Dinners.Domain.Menus;
+using Dinners.Domain.Reservations;
+
+namespace Dinners.Infrastructure.Domain.ReservationsMenus;
+
+internal sealed class ReservationMenus 
+{
+    public ReservationId ReservationId { get; set; }
+
+    public MenuId MenuId { get; set; }
+}
---
+++ src/Modules/Dinners/Infrastructure/Domain/ReservationsMenus/ReservationMenusConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Dinners.Domain.Menus;
+using Dinners.Domain.Reservations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dinners.Infrastructure.Domain.ReservationsMenus;
+
+internal sealed class ReservationMenusConfiguration : IEntityTypeConfiguration<ReservationMenus>
+{
+    public void Configure(EntityTypeBuilder<ReservationMenus> builder)
+    {
+        builder.ToTable("ReservationMenus", "dinners");
+
+        builder.HasKey(x => new { x.ReservationId, x.MenuId });
+
+        builder.Property(p => p.MenuId)
+            .HasConversion(
+                menuId => menuId.Value,
+                value => MenuId.Create(value))
+            .ValueGeneratedNever()
+            .HasColumnName("MenuId");
+
+        builder.Property(p => p.ReservationId)
+            .HasConversion(
+                reservationId => reservationId.Value,
+                value => ReservationId.Create(value))
+            .ValueGeneratedNever()
+            .HasColumnName("ReservationId");
+    }
+}
