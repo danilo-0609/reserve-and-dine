ebc4389a9c22af9b16d0884b8b912d7d4f6f665e
Danilo Usuga <daniusugaj@gmail.com>
2024-03-15 09:57:34 -0500

    Adding event bus implementation}

---
+++ src/Modules/Dinners/Infrastructure/EventsBus/EventBus.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.Application;
+using Dinners.Application.Common;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Dinners.Infrastructure.EventsBus;
+
+internal sealed class EventBus : IEventBus
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+    private readonly ILogger<EventBus> _logger;
+
+    public EventBus(IPublishEndpoint publishEndpoint, ILogger<EventBus> logger)
+    {
+        _publishEndpoint = publishEndpoint;
+        _logger = logger;
+    }
+
+    public async Task PublishAsync<T>(T @event) 
+        where T : IntegrationEvent
+    {
+        _logger.LogInformation("Publishing {@Name}. At {@DateTime}",
+            nameof(@event),
+            DateTime.Now);
+
+        await _publishEndpoint.Publish(@event);
+    }
+}
