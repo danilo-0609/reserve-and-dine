525f568932629a685d7f188a08313967de44d192
Danilo Usuga <daniusugaj@gmail.com>
2024-03-15 09:40:28 -0500

    Adding new Integration Event for rating publish event

---
+++ src/Modules/Dinners/IntegrationEvents/RatingPublishedIntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using BuildingBlocks.Application;
+
+namespace Dinners.IntegrationEvents;
+
+public sealed record RatingPublishedIntegrationEvent(Guid IntegrationEventId,
+    Guid RestaurantRatingId,
+    Guid ClientId,
+    int Stars,
+    Guid RestaurantId,
+    string RestaurantTitle,
+    DateTime OcurredOn) : IntegrationEvent(IntegrationEventId, OcurredOn);
