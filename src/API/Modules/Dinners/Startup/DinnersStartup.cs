1e1e89e5c8c882c8dfc92838b12541efab020ec4
Danilo Usuga <daniusugaj@gmail.com>
2024-03-15 08:50:16 -0500

    Installing dinners module services

---
+++ src/API/Modules/Dinners/Startup/DinnersStartup.cs
@@ -0,0 +1,15 @@
+﻿using Dinners.Application;
+using Dinners.Infrastructure;
+
+namespace API.Modules.Dinners.Startup;
+
+public static class DinnersStartup
+{
+    public static IServiceCollection AddDinners(this IServiceCollection services, string databaseConnectionString)
+    {
+        services.AddApplication();
+        services.AddInfrastructure(databaseConnectionString);
+
+        return services;
+    }
+}
