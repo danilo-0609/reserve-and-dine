eda20923c57eae4e78ae63a79e04802c01e2c2a2
Danilo Usuga <daniusugaj@gmail.com>
2024-03-14 22:08:01 -0500

    Working on Blob services

---
+++ src/Modules/Dinners/Infrastructure/Blobs/MenuBlobService.cs
@@ -0,0 +1,64 @@
+﻿using Azure.Storage.Blobs;
+using BuildingBlocks.Application;
+using Dinners.Application.Common;
+
+namespace Dinners.Infrastructure.Blobs;
+
+internal sealed class MenuBlobService : IMenuBlobService
+{
+    private readonly BlobServiceClient _blobServiceClient;
+    private const string ContainerName = "menus";
+
+    public MenuBlobService(BlobServiceClient blobServiceClient)
+    {
+        _blobServiceClient = blobServiceClient;
+    }
+
+    public async Task DeleteBlobAsync(string name)
+    {
+        var containerClient = _blobServiceClient.GetBlobContainerClient(ContainerName);
+    
+        await containerClient.DeleteBlobIfExistsAsync(name);
+    }
+
+    public async Task<BlobObject?> GetBlobAsync(string name)
+    {
+        try
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(ContainerName);
+
+            var client = containerClient.GetBlobClient(name);
+
+            if (!await client.ExistsAsync())
+            {
+                return null;
+            }
+
+            var content = await client.DownloadAsync();
+
+            var blobProperties = await client.GetPropertiesAsync();
+            string contentType = blobProperties.Value.ContentType;
+
+            MemoryStream memoryStream = new MemoryStream();
+            await content.Value.Content.CopyToAsync(memoryStream);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return new BlobObject(memoryStream, contentType);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public async Task<Uri> UploadFileBlobAsync(string filePath, string fileName)
+    {
+        var containerClient = _blobServiceClient.GetBlobContainerClient(ContainerName);
+
+        var blobClient = containerClient.GetBlobClient(fileName);
+
+        await blobClient.UploadAsync(filePath, true);
+
+        return new Uri(fileName);
+    }
+}
