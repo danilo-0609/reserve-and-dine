fc56c9da69e3a1863031dc2f0d566dc351d48d73
Danilo Usuga <daniusugaj@gmail.com>
2024-03-15 10:00:08 -0500

    Adding db context implementation

---
+++ src/Modules/Dinners/Infrastructure/DinnersDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Dinners.Domain.Menus;
+using Dinners.Domain.Menus.MenuReviews;
+using Dinners.Domain.Reservations;
+using Dinners.Domain.Reservations.Refunds;
+using Dinners.Domain.Reservations.ReservationsPayments;
+using Dinners.Domain.Restaurants.RestaurantRatings;
+using Dinners.Infrastructure.Domain.Menus.MenuReviews;
+using Dinners.Infrastructure.Domain.ReservationsMenus;
+using Dinners.Infrastructure.Domain.Restaurants.RestaurantsRatings;
+using Dinners.Infrastructure.Outbox;
+using Domain.Restaurants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Dinners.Infrastructure;
+
+internal sealed class DinnersDbContext : DbContext, IApplicationDbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public DinnersDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Menu> Menus { get; set; }
+
+    public DbSet<MenusReviews> MenusReviews { get; set; }
+
+    public DbSet<MenuReview> Reviews { get; set; }
+
+    public DbSet<ReservationMenus> ReservationMenus { get; set; }
+
+    public DbSet<Reservation> Reservations { get; set; }
+
+    public DbSet<ReservationPayment> ReservationPayments { get; set; }
+
+    public DbSet<Refund> Refunds { get; set; }
+
+    public DbSet<Restaurant> Restaurants { get; set; }
+
+    public DbSet<RestaurantRatings> RestaurantRatings { get; set; }
+
+    public DbSet<RestaurantRating> Ratings { get; set; }
+
+    public DbSet<OutboxMessage> OutboxMessages { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("Database"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DinnersDbContext).Assembly);
+    }
+}
---
+++ src/Modules/Dinners/Infrastructure/IApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Dinners.Domain.Menus;
+using Dinners.Domain.Menus.MenuReviews;
+using Dinners.Domain.Reservations;
+using Dinners.Domain.Reservations.Refunds;
+using Dinners.Domain.Reservations.ReservationsPayments;
+using Dinners.Domain.Restaurants.RestaurantRatings;
+using Domain.Restaurants;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dinners.Infrastructure;
+
+public interface IApplicationDbContext
+{
+    DbSet<Menu> Menus { get; }
+
+    DbSet<MenuReview> Reviews { get; }
+
+    DbSet<Reservation> Reservations { get; }
+
+    DbSet<ReservationPayment> ReservationPayments { get; }
+
+    DbSet<Refund> Refunds { get; }
+
+    DbSet<Restaurant> Restaurants { get; }
+
+    DbSet<RestaurantRating> Ratings { get; }  
+}
