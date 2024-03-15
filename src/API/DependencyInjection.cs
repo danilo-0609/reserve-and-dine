81a7be89ce4a63e4222697124c6b8239e359ae6e
Danilo Usuga <daniusugaj@gmail.com>
2024-03-15 08:51:17 -0500

    Installing presentation and dinners module services

---
+++ src/API/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Storage.Blobs;
+
+namespace API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services, string azureBlobStorageConnectionString)
+    {
+        services.AddSingleton(x => new BlobServiceClient(azureBlobStorageConnectionString));
+
+        return services;
+    }
+}
--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,15 +1,25 @@
+using API;
+using API.Configuration;
+using API.Modules.Dinners.Startup;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Presentation service
+builder.Services.AddPresentation(await ConnectionsManager.GetAzureBlobStorageConnectionString());
+
+//Modules services
+builder.Services.AddDinners(await ConnectionsManager.GetDatabaseConnectionString());
+
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
