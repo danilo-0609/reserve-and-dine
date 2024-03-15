42b69474352b2a3eed70b602d8826180b93c3233
Danilo Usuga <daniusugaj@gmail.com>
2024-03-15 09:46:40 -0500

    Adding MenusReviews entity for being a join table between Menus and Reviews

---
+++ src/Modules/Dinners/Infrastructure/Domain/Menus/MenuReviews/MenusReviews.cs
@@ -0,0 +1,11 @@
+﻿using Dinners.Domain.Menus;
+using Dinners.Domain.Menus.MenuReviews;
+
+namespace Dinners.Infrastructure.Domain.Menus.MenuReviews;
+
+public sealed class MenusReviews
+{
+    public MenuId MenuId { get; }
+
+    public MenuReviewId MenuReviewId { get; }
+}
---
+++ src/Modules/Dinners/Infrastructure/Domain/Menus/MenuReviews/MenusReviewsConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Dinners.Domain.Menus;
+using Dinners.Domain.Menus.MenuReviews;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dinners.Infrastructure.Domain.Menus.MenuReviews;
+
+internal sealed class MenusReviewsConfiguration : IEntityTypeConfiguration<MenusReviews>
+{
+    public void Configure(EntityTypeBuilder<MenusReviews> builder)
+    {
+        builder.ToTable("MenusReviews", "dinners");
+
+        builder.HasKey(r => new { r.MenuId, r.MenuReviewId });
+    
+        builder.Property(r => r.MenuId)
+            .HasConversion(
+                menuId => menuId.Value,
+                value => MenuId.Create(value))
+            .ValueGeneratedNever()
+            .HasColumnName("MenuId");
+
+        builder.Property(r => r.MenuReviewId)
+            .HasConversion(
+                menuReviewId => menuReviewId.Value,
+                value => MenuReviewId.Create(value))
+            .ValueGeneratedNever()
+            .HasColumnName("MenuReviewId");
+    }
+}
