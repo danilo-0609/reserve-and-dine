2d2cce7870d70c259a798c7453d032c44c718788
Danilo Usuga <daniusugaj@gmail.com>
2024-03-25 22:04:04 -0500

    Adding SuccessOperation class for returning this value when business operations are succesful but do not return any value to the user

---
+++ src/BuildingBlocks/Domain/Results/SuccessOperation.cs
@@ -0,0 +1,13 @@
+﻿namespace BuildingBlocks.Domain.Results;
+
+public sealed record SuccessOperation
+{
+    private string _value;
+
+    public static SuccessOperation Code => new SuccessOperation(nameof(Code));
+
+    private SuccessOperation(string value)
+    {
+        _value = value;
+    }
+}
