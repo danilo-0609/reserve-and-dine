b6f86b03883b6358356d4510739288e6d8ea131d
Danilo Usuga <daniusugaj@gmail.com>
2024-03-25 21:58:34 -0500

    Adding a new day of operation class, and refactoring RestaurantSchedule ValueObject

---
+++ src/Modules/Dinners/Domain/Restaurants/RestaurantSchedules/DayOfOperation.cs
@@ -0,0 +1,13 @@
+﻿namespace Dinners.Domain.Restaurants.RestaurantSchedules;
+
+public sealed record DayOfOperation
+{
+    public DayOfWeek DayOfWeek { get; private set; }
+
+    public DayOfOperation(DayOfWeek dayOfWeek)
+    {
+        DayOfWeek = dayOfWeek;
+    }
+
+    private DayOfOperation() { }
+}
--- src/Modules/Dinners/Domain/Restaurants/RestaurantSchedules/RestaurantSchedule.cs
+++ src/Modules/Dinners/Domain/Restaurants/RestaurantSchedules/RestaurantSchedule.cs
@@ -4,21 +4,29 @@ namespace Dinners.Domain.Restaurants.RestaurantSchedules;
 
 public sealed record RestaurantSchedule
 {
-    public List<DayOfWeek> Days { get; set; }
+    public DayOfOperation Day { get; set; }
 
     public TimeRange HoursOfOperation { get; private set; }
 
-    private RestaurantSchedule(List<DayOfWeek> days, TimeRange hoursOfOperation)
+    public DateTime? ReopeningTime { get; private set; }
+
+    private RestaurantSchedule(DayOfOperation days, TimeRange hoursOfOperation, DateTime? reopeningTime)
     {
-        Days = days;
+        Day = days;
         HoursOfOperation = hoursOfOperation;
+        ReopeningTime = reopeningTime;
     }
 
-    public static RestaurantSchedule Create(List<DayOfWeek> days,
-        TimeSpan start,
-        TimeSpan end)
+    public static RestaurantSchedule Create(DayOfWeek day,
+        DateTime start,
+        DateTime end)
     {
-        return new RestaurantSchedule(days, new TimeRange(start, end));
+        return new RestaurantSchedule(new DayOfOperation(day), new TimeRange(start, end), null);
+    }
+    
+    public void EstablishReopeningTime(DateTime reopeningTime)
+    {
+        ReopeningTime = reopeningTime;
     }
 
     private RestaurantSchedule() { }
