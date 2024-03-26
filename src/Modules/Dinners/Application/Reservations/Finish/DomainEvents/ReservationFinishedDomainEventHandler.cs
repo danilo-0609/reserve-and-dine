526b9adeb502a16094d43ab97330a12bf3d95909
Danilo Usuga <daniusugaj@gmail.com>
2024-03-26 16:41:15 -0500

    Moving crucial business logic from domain event handler to command handler

--- src/Modules/Dinners/Application/Reservations/Finish/DomainEvents/ReservationFinishedDomainEventHandler.cs
+++ src/Modules/Dinners/Application/Reservations/Finish/DomainEvents/ReservationFinishedDomainEventHandler.cs
@@ -1,42 +1,12 @@
 ﻿using BuildingBlocks.Application;
-using Dinners.Application.Common;
 using Dinners.Domain.Reservations.DomainEvents;
-using Dinners.Domain.Restaurants;
-using Dinners.Domain.Restaurants.Errors;
-using Domain.Restaurants;
 
 namespace Dinners.Application.Reservations.Finish.DomainEvents;
 
 internal sealed class ReservationFinishedDomainEventHandler : IDomainEventHandler<ReservationFinishedDomainEvent>
 {
-    private readonly IRestaurantRepository _restaurantRepository;
-    private readonly IUnitOfWork _unitOfWork;
-
-    public ReservationFinishedDomainEventHandler(IRestaurantRepository restaurantRepository, IUnitOfWork unitOfWork)
+    public Task Handle(ReservationFinishedDomainEvent notification, CancellationToken cancellationToken)
     {
-        _restaurantRepository = restaurantRepository;
-        _unitOfWork = unitOfWork;
-    }
-
-    public async Task Handle(ReservationFinishedDomainEvent notification, CancellationToken cancellationToken)
-    {
-        Restaurant? restaurant = await _restaurantRepository.GetRestaurantById(notification.RestaurantId);
-
-        if (restaurant is null)
-        {
-            throw new DomainEventHandlerException(RestaurantErrorCodes.NotFound, DateTime.UtcNow);
-        }
-
-        restaurant.AddRestaurantClient(notification.ClientId);
-
-        var tableFree = restaurant.FreeTable(notification.NumberOfTable);
-
-        if (tableFree.IsError)
-        {
-            throw new DomainEventHandlerException(tableFree.FirstError, DateTime.UtcNow);
-        }
-
-        await _restaurantRepository.UpdateAsync(restaurant);
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        throw new NotImplementedException();
     }
 }
--- src/Modules/Dinners/Application/Reservations/Finish/FinishReservationCommandHandler.cs
+++ src/Modules/Dinners/Application/Reservations/Finish/FinishReservationCommandHandler.cs
@@ -3,16 +3,20 @@ using Dinners.Domain.Reservations.Errors;
 using Dinners.Domain.Reservations;
 using ErrorOr;
 using MediatR;
+using Dinners.Domain.Restaurants;
+using Dinners.Domain.Restaurants.Errors;
 
 namespace Dinners.Application.Reservations.Finish;
 
 internal sealed class FinishReservationCommandHandler : ICommandHandler<FinishReservationCommand, ErrorOr<Unit>>
 {
     private readonly IReservationRepository _reservationRepository;
+    private readonly IRestaurantRepository _restaurantRepository;
 
-    public FinishReservationCommandHandler(IReservationRepository reservationRepository)
+    public FinishReservationCommandHandler(IReservationRepository reservationRepository, IRestaurantRepository restaurantRepository)
     {
         _reservationRepository = reservationRepository;
+        _restaurantRepository = restaurantRepository;
     }
 
     public async Task<ErrorOr<Unit>> Handle(FinishReservationCommand request, CancellationToken cancellationToken)
@@ -24,16 +28,30 @@ internal sealed class FinishReservationCommandHandler : ICommandHandler<FinishRe
             return ReservationErrorsCodes.NotFound;
         }
 
-        ErrorOr<ReservationStatus> finishReservation = reservation.Finish();
+        var finishReservation = reservation.Finish();
 
         if (finishReservation.IsError)
         {
             return finishReservation.FirstError;
         }
 
+        var restaurant = await _restaurantRepository.GetRestaurantById(reservation.RestaurantId);
+
+        if (restaurant is null)
+        {
+            return RestaurantErrorCodes.NotFound;
+        }
+
+        var freeTable = restaurant.FreeTable(reservation.ReservationInformation.ReservedTable);
+
+        if (freeTable.IsError)
+        {
+            return freeTable.FirstError;
+        }
+
         var reservationUpdate = reservation.Update(reservation.ReservationInformation,
             reservation.MenuIds.ToList(),
-            finishReservation.Value,
+            reservation.ReservationStatus,
             reservation.ReservationAttendees,
             reservation.ReservationPaymentId,
             reservation.RefundId);
