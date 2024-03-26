e810be17b48758c0aea6b7b6db01f08fe1a68c01
Danilo Usuga <daniusugaj@gmail.com>
2024-03-26 16:44:44 -0500

    Refactoring handler

--- src/Modules/Dinners/Application/Reservations/Visit/VisitReservationCommandHandler.cs
+++ src/Modules/Dinners/Application/Reservations/Visit/VisitReservationCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Dinners.Application.Common;
 using Dinners.Domain.Reservations;
 using Dinners.Domain.Reservations.Errors;
+using Dinners.Domain.Restaurants;
+using Dinners.Domain.Restaurants.Errors;
 using ErrorOr;
 using MediatR;
 
@@ -9,10 +11,12 @@ namespace Dinners.Application.Reservations.Visit;
 internal sealed class VisitReservationCommandHandler : ICommandHandler<VisitReservationCommand, ErrorOr<Unit>>
 {
     private readonly IReservationRepository _reservationRepository;
+    private readonly IRestaurantRepository _restaurantRepository;
 
-    public VisitReservationCommandHandler(IReservationRepository reservationRepository)
+    public VisitReservationCommandHandler(IReservationRepository reservationRepository, IRestaurantRepository restaurantRepository)
     {
         _reservationRepository = reservationRepository;
+        _restaurantRepository = restaurantRepository;
     }
 
     public async Task<ErrorOr<Unit>> Handle(VisitReservationCommand request, CancellationToken cancellationToken)
@@ -24,21 +28,36 @@ internal sealed class VisitReservationCommandHandler : ICommandHandler<VisitRese
             return ReservationErrorsCodes.NotFound;
         }
 
-        ErrorOr<ReservationStatus> asisting = reservation.Visit();
+        var asisting = reservation.Visit();
     
         if (asisting.IsError)
         {
             return asisting.FirstError;
         }
 
+        var restaurant = await _restaurantRepository.GetRestaurantById(reservation.RestaurantId);
+
+        if (restaurant is null)
+        {
+            return RestaurantErrorCodes.NotFound;
+        }
+
+        var occupyTable = restaurant.OccupyTable(reservation.ReservationInformation.ReservedTable);
+
+        if (occupyTable.IsError)
+        {
+            return occupyTable.FirstError;
+        }
+
         var reservationUpdate = reservation.Update(reservation.ReservationInformation,
             reservation.MenuIds.ToList(),
-            asisting.Value,
+            reservation.ReservationStatus,
             reservation.ReservationAttendees,
             reservation.ReservationPaymentId!,
             reservation.RefundId);
 
         await _reservationRepository.UpdateAsync(reservationUpdate, cancellationToken);
+        await _restaurantRepository.UpdateAsync(restaurant);
 
         return Unit.Value;
     }
