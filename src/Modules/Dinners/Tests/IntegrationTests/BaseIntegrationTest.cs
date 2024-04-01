d0f8dd3094f45550ca9d81360155735ea8db2742
Danilo Usuga <daniusugaj@gmail.com>
2024-04-01 17:07:01 -0500

    Adding base abstract class for integration testing that implements MediatR sender and DbContext

---
+++ src/Modules/Dinners/Tests/IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using Dinners.Infrastructure;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dinners.Tests.IntegrationTests;
+
+public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestWebAppFactory>
+{
+    private readonly IServiceScope _scope;
+    protected readonly ISender Sender;
+    protected readonly DinnersDbContext DbContext;
+
+    protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
+    {
+        _scope = factory.Services.CreateScope();    
+    
+        Sender = _scope.ServiceProvider.GetRequiredService<ISender>();
+        DbContext = _scope.ServiceProvider.GetRequiredService<DinnersDbContext>();
+    }
+}
