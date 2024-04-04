7ab2decdb609ee2910c22b2a9cc89c278ad91ace
Danilo Usuga <daniusugaj@gmail.com>
2024-04-03 20:32:16 -0500

    Adding AsNoTracking method in GetById method

--- src/Modules/Dinners/Infrastructure/Domain/Menus/MenuRepository.cs
+++ src/Modules/Dinners/Infrastructure/Domain/Menus/MenuRepository.cs
@@ -22,6 +22,7 @@ internal sealed class MenuRepository : IMenuRepository
     {
         return await _dbContext
             .Menus
+            .AsNoTracking()
             .Where(r => r.Id == menuId)
             .SingleOrDefaultAsync();
     }
