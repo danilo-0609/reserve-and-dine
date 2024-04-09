ef169459d114ed835d649872db4ee231a599d344
Danilo Usuga <daniusugaj@gmail.com>
2024-04-08 20:35:59 -0500

    Adding custom ProblemErrors methods to return specific errors messages and formats

---
+++ src/API/Configuration/ProblemError.cs
@@ -0,0 +1,56 @@
+﻿using API.Common.Http;
+using ErrorOr;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+namespace API.Configuration;
+
+public sealed class ProblemError 
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public ProblemError(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public IResult Errors(List<Error> errors)
+    {
+        if (errors.Count == 0)
+        {
+            return Results.Problem();
+        }
+
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        _httpContextAccessor.HttpContext!.Items[HttpContextItemKeys.Errors] = errors;
+
+        return Problem(errors[0]);
+    }
+
+    private IResult Problem(Error error)
+    {
+        int statusCode = error.Type switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Results.Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private IResult ValidationProblem(List<Error> errors)
+    {
+        Dictionary<string, string[]> modelStateDictionary = new();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.Add(error.Code, new string[] { error.Description });
+        }
+
+        return Results.ValidationProblem(modelStateDictionary);
+    }
+}
