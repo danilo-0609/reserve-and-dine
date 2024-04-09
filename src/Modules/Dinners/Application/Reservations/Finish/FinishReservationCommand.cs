dd05f50373a99766a544f364d6dd367fe5733446
Danilo Usuga <daniusugaj@gmail.com>
2024-04-08 20:44:01 -0500

    Making class public

--- src/Modules/Dinners/Application/Reservations/Finish/FinishReservationCommand.cs
+++ src/Modules/Dinners/Application/Reservations/Finish/FinishReservationCommand.cs
@@ -4,4 +4,4 @@ using MediatR;
 
 namespace Dinners.Application.Reservations.Finish;
 
-internal sealed record FinishReservationCommand(Guid ReservationId) : ICommand<ErrorOr<Unit>>;
+public sealed record FinishReservationCommand(Guid ReservationId) : ICommand<ErrorOr<Unit>>;
