fb85adf24a6ee2533718ff19323f6854bb850558
Danilo Usuga <daniusugaj@gmail.com>
2024-04-08 20:37:46 -0500

    Working on Reservation's aggregate endpoints

---
+++ src/API/Modules/Dinners/Endpoints/Reservations/ReservationsModule.cs
@@ -0,0 +1,88 @@
+﻿using API.Configuration;
+using API.Modules.Dinners.Requets;
+using Carter;
+using Dinners.Application.Reservations.Cancel;
+using Dinners.Application.Reservations.Finish;
+using Dinners.Application.Reservations.GetById;
+using Dinners.Application.Reservations.Payments.Pays;
+using Dinners.Application.Reservations.Request;
+using Dinners.Application.Reservations.Visit;
+using MediatR;
+
+namespace API.Modules.Dinners.Endpoints.Reservations;
+
+public sealed class ReservationsModule : CarterModule
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public ReservationsModule(IHttpContextAccessor httpContextAccessor)
+        : base("/reservations")
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetReservationByIdQuery(id));
+
+            return result.Match(
+                onValue => Results.Ok(onValue),
+                onError => new ProblemError(_httpContextAccessor).Errors(onError));
+        });
+
+        app.MapPost("/request", async (RequestReservationRequest request, ISender sender) =>
+        {
+            var result = await sender.Send(new RequestReservationCommand(request.ReservedTable,
+                request.Price,
+                request.Currency,
+                request.StartReservationDateTime,
+                request.EndReservationDateTime,
+                request.RestaurantId,
+                request.Name,
+                request.NumberOfAttendees,
+                request.MenuIds));
+
+            return result.Match(
+                onValue => Results.Created(onValue.ToString(), onValue),
+                onError => new ProblemError(_httpContextAccessor).Errors(onError));
+        });
+
+        app.MapPut("/cancel", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new CancelReservationCommand(id));
+
+            return result.Match(
+                onValue => Results.NoContent(),
+                onError => new ProblemError(_httpContextAccessor).Errors(onError));
+        });
+
+        app.MapPost("/assist", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new VisitReservationCommand(id));
+
+            return result.Match(
+                onValue => Results.NoContent(),
+                onError => new ProblemError(_httpContextAccessor).Errors(onError));
+        });
+
+        app.MapPut("/pay", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new PayReservationCommand(id));
+
+            return result.Match(
+                onValue => Results.NoContent(),
+                onError => new ProblemError(_httpContextAccessor).Errors(onError));
+        });
+
+        app.MapPut("/finish", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new FinishReservationCommand(id));
+
+            return result.Match(
+                onValue => Results.NoContent(),
+                onError => new ProblemError(_httpContextAccessor).Errors(onError));
+        });
+    }
+}
---
+++ src/API/Modules/Dinners/Requets/RequestReservationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Modules.Dinners.Requets;
+
+public sealed record RequestReservationRequest(int ReservedTable,
+        decimal Price,
+        string Currency,
+        DateTime StartReservationDateTime,
+        DateTime EndReservationDateTime,
+        Guid RestaurantId,
+        string Name,
+        int NumberOfAttendees,
+        List<Guid> MenuIds);
