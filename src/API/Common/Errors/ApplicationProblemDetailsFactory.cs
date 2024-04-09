cae9a2116e519877ccfdbd48214f0fd35f374485
Danilo Usuga <daniusugaj@gmail.com>
2024-04-08 20:34:42 -0500

    Adding ProblemDetailsFactory to use it in endpoints responses

---
+++ src/API/Common/Errors/ApplicationProblemDetailsFactory.cs
@@ -0,0 +1,94 @@
+﻿using API.Common.Http;
+using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Diagnostics;
+
+namespace API.Common.Errors;
+
+public sealed class ApplicationProblemDetailsFactory : ProblemDetailsFactory
+{
+    private readonly ApiBehaviorOptions _options;
+
+    public ApplicationProblemDetailsFactory(ApiBehaviorOptions options)
+    {
+        _options = options;
+    }
+
+    public override ProblemDetails CreateProblemDetails(
+        HttpContext httpContext, 
+        int? statusCode = null, 
+        string? title = null, 
+        string? type = null, 
+        string? detail = null, 
+        string? instance = null)
+    {
+        statusCode ??= 500;
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = statusCode,
+            Title = title,
+            Type = type,
+            Detail = detail,
+            Instance = instance
+        };
+
+        ApplyProblemDetailsDefault(httpContext, problemDetails, statusCode.Value);
+
+        return problemDetails;
+    }
+
+    public override ValidationProblemDetails CreateValidationProblemDetails(
+        HttpContext httpContext, 
+        ModelStateDictionary modelStateDictionary, 
+        int? statusCode = null, 
+        string? title = null, 
+        string? type = null, 
+        string? detail = null, 
+        string? instance = null)
+    {
+        statusCode ??= 400;
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = statusCode,
+            Title = title,
+            Type = type,
+            Detail = detail,
+        };
+
+        ApplyProblemDetailsDefault(httpContext, problemDetails, statusCode.Value);
+
+        return (ValidationProblemDetails)problemDetails;
+    }
+
+    private void ApplyProblemDetailsDefault(
+        HttpContext httpContext,
+        ProblemDetails problemDetails,
+        int statusCode)
+    {
+        problemDetails.Status ??= statusCode;
+
+        if (_options.ClientErrorMapping.TryGetValue(statusCode, out var mapping))
+        {
+            problemDetails.Title ??= mapping.Title;
+            problemDetails.Type ??= mapping.Link;
+        }
+
+        string traceId = Activity.Current?.Id ?? httpContext.TraceIdentifier;
+
+        if (traceId is not null)
+        {
+            problemDetails.Extensions["traceId"] = traceId;
+        }
+
+        var errors = httpContext?.Items[HttpContextItemKeys.Errors] as List<Error>;
+
+        if (errors is not null)
+        {
+            problemDetails.Extensions.Add("errorCodes", errors.Select(error => error.Code));
+        }
+    }
+}
---
+++ src/API/Common/Http/HttpContextItemKeys.cs
@@ -0,0 +1,6 @@
+﻿namespace API.Common.Http;
+
+public class HttpContextItemKeys
+{
+    public const string Errors = "errors";
+}
