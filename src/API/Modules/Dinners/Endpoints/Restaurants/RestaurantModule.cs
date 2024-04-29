c3c2578d8a4d53f88b7c506690de61df127f1673
Danilo Úsuga <126912602+danilo-0609@users.noreply.github.com>
2024-04-29 18:07:19 -0500

    Creating restaurant module for aggregate endpoints

---
+++ src/API/Modules/Dinners/Endpoints/Restaurants/RestaurantModule.cs
@@ -0,0 +1,17 @@
+﻿using Carter;
+
+namespace API.Modules.Dinners.Endpoints.Restaurants;
+
+public sealed class RestaurantModule : CarterModule
+{
+    public RestaurantModule()
+        : base("/restaurants")
+    {
+        
+    }
+
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        
+    }
+}
