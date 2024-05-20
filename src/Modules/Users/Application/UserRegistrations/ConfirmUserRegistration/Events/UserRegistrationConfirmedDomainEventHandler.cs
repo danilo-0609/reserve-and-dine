1dc864bf4cececdccec79e7a9dd79c793e6879c6
Danilo Úsuga <126912602+danilo-0609@users.noreply.github.com>
2024-05-20 11:47:33 -0500

    Adding logging

--- src/Modules/Users/Application/UserRegistrations/ConfirmUserRegistration/Events/UserRegistrationConfirmedDomainEventHandler.cs
+++ src/Modules/Users/Application/UserRegistrations/ConfirmUserRegistration/Events/UserRegistrationConfirmedDomainEventHandler.cs
@@ -1,26 +1,37 @@
-﻿using BuildingBlocks.Application;
+﻿using Dapper;
+using MediatR;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Users.Application.Common;
+using Users.Application.Services;
 using Users.Domain.UserRegistrations;
 using Users.Domain.UserRegistrations.Events;
 using Users.Domain.Users;
 
 namespace Users.Application.UserRegistrations.ConfirmUserRegistration.Events;
 
-internal sealed class UserRegistrationConfirmedDomainEventHandler : IDomainEventHandler<UserRegistrationConfirmedDomainEvent>
+internal sealed class UserRegistrationConfirmedDomainEventHandler : INotificationHandler<UserRegistrationConfirmedDomainEvent>
 {
     private readonly IUserRegistrationRepository _userRegistrationRepository;
+    private readonly ILogger<UserRegistrationConfirmedDomainEventHandler> _logger;
     private readonly IUserRepository _userRepository;
     private readonly IUnitOfWork _unitOfWork;
 
-    public UserRegistrationConfirmedDomainEventHandler(IUserRegistrationRepository userRegistrationRepository, IUserRepository userRepository, IUnitOfWork unitOfWork)
+    public UserRegistrationConfirmedDomainEventHandler(IUserRegistrationRepository userRegistrationRepository, ILogger<UserRegistrationConfirmedDomainEventHandler> logger, IUserRepository userRepository, IUnitOfWork unitOfWork)
     {
         _userRegistrationRepository = userRegistrationRepository;
+        _logger = logger;
         _userRepository = userRepository;
         _unitOfWork = unitOfWork;
     }
 
     public async Task Handle(UserRegistrationConfirmedDomainEvent domainEvent, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Working on {@Name}, at {@DateTime}", 
+            nameof(UserRegistrationConfirmedDomainEventHandler),
+            DateTime.Now);
+
         var userRegistration = await _userRegistrationRepository.GetByIdAsync(domainEvent.Id);
     
         if (userRegistration is null)
