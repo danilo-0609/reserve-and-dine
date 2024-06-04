40cc918122f0f7cbc2737786c068993fc0b9ed00
danilo0609 <daniusugaj@gmail.com>
2024-06-04 07:21:40 -0500

    Creating a class to facilitate ef core migrations

---
+++ src/Modules/Dinners/Infrastructure/Configuration/DatabaseDesignTimeDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System.Configuration;
+
+namespace Dinners.Infrastructure.Configuration;
+
+public class DatabaseDesignTimeDbContextFactory : IDesignTimeDbContextFactory<DinnersDbContext>
+{
+    private readonly IConfiguration _configuration;
+
+    public DatabaseDesignTimeDbContextFactory(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public DatabaseDesignTimeDbContextFactory()
+    {
+    }
+
+    public DinnersDbContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<DinnersDbContext>();
+
+        builder.UseSqlServer(_configuration.GetConnectionString("DockerSqlDatabase"));
+        
+        return new DinnersDbContext(builder.Options);
+    }
+}
